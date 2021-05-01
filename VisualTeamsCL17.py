2443caac37d717b1cf18b39e7c42101b0204a9ca
PavelQC <69416733+PavelQuiquia@users.noreply.github.com>
2021-05-01 10:12:31 +0200

    Delete VisualTeamsCL17.py

--- VisualTeamsCL17.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as py
-import matplotlib.pyplot as plt
-
-
-T = pd.read_csv(r'C:\Users\Pavel\Desktop\Python1\Champions League\Season17-18\TeamsCL17.csv')
-T.sort_values(by=['goal'], ascending=False).plot.bar(x='teamRegionName', y='goal')
